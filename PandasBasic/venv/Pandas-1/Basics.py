32de3b4bc08d538e2b485aab364a106d92d4a408
Maxyee <eyaminkhan00@gmail.com>
2018-03-05 03:38:56 +0600

    Learning Data Analysis using pandas library of python

---
+++ PandasBasic/venv/Pandas-1/Basics.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+
+import numpy as np
+
+web_stats = {'Day':[1,2,3,4,5,6],
+             'Visitors':[43,53,34,45,64,34],
+             'Bounce_Rate':[65,72,62,64,54,66]}
+
+df = pd.DataFrame(web_stats)
+
+
+print(df)
+
+print(df.head())
+print(df.tail())
+print(df.tail(2))
+
+print(df.set_index('Day'))
+
+df2 = df.set_index('Day')
+print(df2.head())
+
+df.set_index('Day', inplace=True)
+print(df.head())
+
+
+print(df['Visitors'])
+print(df.Visitors)
+
+print(df[['Bounce_Rate','Visitors']])
+
+print(df.Visitors.tolist())
+print(np.array(df[['Bounce_Rate','Visitors']]))
+
+df3 = pd.DataFrame(np.array(df[['Bounce_Rate','Visitors']]))
+print(df3)
